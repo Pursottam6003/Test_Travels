94faa25f602b6fdf242fa7eff90e246daa2fcfa2
Pursottam6003 <rahulsah6003@gmail.com>
2022-06-26 20:52:06 +0530

    update files

---
+++ precache-manifest.e17e10ab0cff01091b1c0ab07bf0c0aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d032cc3703cd5abd281c0d7c0d90de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05e34535acfd226989f7",
+    "url": "/static/css/main.2044c8a7.chunk.css"
+  },
+  {
+    "revision": "15a2fe9ad89c592aaf6f",
+    "url": "/static/js/2.68978b41.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.68978b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05e34535acfd226989f7",
+    "url": "/static/js/main.d642e854.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
